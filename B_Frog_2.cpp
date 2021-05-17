fed7390003cf18b054e5d8ff87055654dd6c1e74
sanath-acharya <sanath123sajipa@gmail.com>
2021-05-17 23:13:50 +0530

    B frog question

---
+++ B_Frog_2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll= long long;
+const int INF=1e9+5;
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+   int n,k;
+   cin>>n;
+   cin>>k;
+    vector<int> h(n);
+   for(int i=0;i<n;i++ ){
+       cin>>h[i];
+   }
+//    for(int i=0;i<n;i++)
+//    cout<<h[i];
+
+    vector<int>dp(n,INF);
+    dp[0]=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<=i+k;j++){
+        if(j<n)    
+        dp[j]=min(dp[j],dp[i]+abs(h[i]-h[j]));
+        }
+    }
+
+    cout<<dp[n-1]<<endl;
+
+}
